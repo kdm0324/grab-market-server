d6f0ff35d8f84d36ee199e099aa35edf8bed03c8
dongmin.kwak <dongmin.kwak@lgepartner.com>
2021-12-23 00:26:44 +0900

    add DB

--- server.js
+++ server.js
@@ -2,18 +2,82 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8080;
+const models = require("./models");
 
 app.use(express.json());
 app.use(cors()); //모든 브라우저에서 내 서버에 요청할 수 있음.
 
 app.get("/products", (req, res) => {
-  res.send("업로드 된 상품들입니다.");
+  const query = req.query;
+  console.log("QUERY : ", query);
+
+  res.send({
+    products: [
+      {
+        id: 1,
+        name: "농구공",
+        price: 100000,
+        seller: "조던",
+        imageUrl: "images/products/basketball1.jpeg",
+      },
+      {
+        id: 2,
+        name: "축구공",
+        price: 50000,
+        seller: "메시",
+        imageUrl: "images/products/soccerball1.jpg",
+      },
+      {
+        id: 3,
+        name: "키보드",
+        price: 10000,
+        seller: "그랩",
+        imageUrl: "images/products/keyboard1.jpg",
+      },
+    ],
+  });
 });
 
 app.post("/products", (req, res) => {
-  res.send("상품이 등록되었습니다.");
+  const body = req.body;
+  const { name, description, price, seller } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요.");
+  }
+  models.Products.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다.");
+    });
+});
+
+app.get("/products/:id/event/:eventId", (req, res) => {
+  const params = req.params;
+  const { id, eventId } = params;
+  res.send(`id는 ${id}와 ${eventId}입니다`);
 });
 
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 에러ㅠ");
+      process.exit();
+    });
 });

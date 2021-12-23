58a67891a8c6d4dd6a4ff21b889e860ca9ba403c
dongmin.kwak <dongmin.kwak@lgepartner.com>
2021-12-24 01:55:51 +0900

    add Database

--- server.js
+++ server.js
@@ -7,44 +7,33 @@ const models = require("./models");
 app.use(express.json());
 app.use(cors()); //모든 브라우저에서 내 서버에 요청할 수 있음.
 
+//전체 조회 API 구현
 app.get("/products", (req, res) => {
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+  })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   const query = req.query;
   console.log("QUERY : ", query);
-
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "메시",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 10000,
-        seller: "그랩",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
 });
 
+//INSERT API 구현
 app.post("/products", (req, res) => {
   const body = req.body;
   const { name, description, price, seller } = body;
   if (!name || !description || !price || !seller) {
     res.send("모든 필드를 입력해주세요.");
   }
-  models.Products.create({
+  models.Product.create({
     name,
     description,
     price,
@@ -62,12 +51,26 @@ app.post("/products", (req, res) => {
     });
 });
 
-app.get("/products/:id/event/:eventId", (req, res) => {
+//상품 상세 조회 API 구현
+app.get("/products/:id", (req, res) => {
   const params = req.params;
-  const { id, eventId } = params;
-  res.send(`id는 ${id}와 ${eventId}입니다`);
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 조회에 에러가 발생했습니다.");
+    });
 });
-
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
   models.sequelize

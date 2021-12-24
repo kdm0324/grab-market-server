52001671a6d301c7389375afde5b4919b1bd8002
dongmin.kwak <dongmin.kwak@lgepartner.com>
2021-12-24 15:16:13 +0900

    add Api for product upload

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 app.use(express.json());
 app.use(cors()); //모든 브라우저에서 내 서버에 요청할 수 있음.
+app.use("/uploads", express.static("uploads"));
 
 //전체 조회 API 구현
 app.get("/products", (req, res) => {
@@ -29,15 +41,16 @@ app.get("/products", (req, res) => {
 //INSERT API 구현
 app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, description, price, seller } = body;
-  if (!name || !description || !price || !seller) {
-    res.send("모든 필드를 입력해주세요.");
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요.");
   }
   models.Product.create({
     name,
     description,
     price,
     seller,
+    imageUrl,
   })
     .then((result) => {
       console.log("상품 생성 결과 : ", result);
@@ -47,8 +60,16 @@ app.post("/products", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 업로드에 문제가 발생했습니다.");
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
     });
+});
+
+//이미지 업로드 api 구현
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 //상품 상세 조회 API 구현
@@ -68,7 +89,7 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 조회에 에러가 발생했습니다.");
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
     });
 });
 app.listen(port, () => {
